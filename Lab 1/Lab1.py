bedbf968d2349e07b435b46d8972cec70fcd0893
Mark <mth513@mun.ca>
2020-02-06 16:40:43 -0330

    Added intersection function

--- Lab 1/Lab1.py
+++ Lab 1/Lab1.py
@@ -8,6 +8,22 @@ Created on Mon Feb  3 01:32:57 2020
 import cv2
 import numpy as np
 
+
+def get_intersections(line_a, line_b):
+
+    rho1, theta1 = line_a[0]
+    rho2, theta2 = line_b[0]
+    A = np.array([
+        [np.cos(theta1), np.sin(theta1)],
+        [np.cos(theta2), np.sin(theta2)]
+    ])
+    b = np.array([[rho1], [rho2]])
+    x0, y0 = np.linalg.solve(A, b)
+    x0, y0 = int(np.round(x0)), int(np.round(y0))
+
+    return [[x0, y0]]
+
+
 vid = cv2.VideoCapture('Movie1.MOV')    # Read Video file
 success, img = vid.read()   # Read video frame, return true if successful
 frame_num = 0
@@ -15,6 +31,8 @@ frame_num = 0
 while success:
     success, img = vid.read()
     if frame_num % 30 == 0:     # Process every 30th video frame
+        vert_lines = []
+        hor_lines = []
 
         size = img.shape
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -25,7 +43,6 @@ while success:
         try:
             num = len(lines)
         except TypeError:
-            print('No lines detected in frame number {}'.format(frame_num))
             continue
 
         for n in range(num):    # Convert polar lines to cartesian
@@ -41,8 +58,19 @@ while success:
 
             if 1.2 <= theta <= 1.9:     # Separate horizontal and vertical lines
                 cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                vert_lines.append(lines[n])
+
             else:
                 cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                hor_lines.append(lines[n])
+
+            for i in range(len(vert_lines)):
+                for j in range(len(hor_lines)):
+                    line1 = vert_lines[i]
+                    line2 = hor_lines[j]
+                    intersections = get_intersections(line1, line2)
+                    x, y = intersections[0]
+                    cv2.circle(img, (x, y), 8, (0, 0, 0), thickness=-1)
 
         cv2.imshow('Image', img)
         cv2.waitKey(0)
