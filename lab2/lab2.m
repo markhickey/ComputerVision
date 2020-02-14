9c03bc30b30d6ffddbdb617bf0b7319248b11d8a
ZacharySheppard <zacharysheppard813@gmail.com>
2020-02-14 17:42:52 -0330

    latest update

--- lab2/lab2.m
+++ lab2/lab2.m
@@ -24,57 +24,54 @@ b = zeros(length(Im.y),1);
 
 p =zeros(Im.x,Im.y,diagLen);
 maxVal = zeros(diagLen,1);
+centers = zeros(Im.x,Im.y);
 
 
-for r = 1:45
-    maxScore = 8*r;
+for r = 1:100
+    maxScore = r^2;
     
     for i = 1: Im.x
-        for j =1: Im.y            
-            if I_edge(i,j) == 1                
-                for tht = 1:360                   
-                   a = round(i-r*cosd(tht));
-                   b = round(j-r*sind(tht));  
-                   
-                      if (a > 0) && (a < Im.x) && (b > 0) && (b <Im.y)
+        for j =1: Im.y
+            if I_edge(i,j) == 1
+                for tht = 1:360
+                    a = round(i-r*cosd(tht));
+                    b = round(j-r*sind(tht));
+                    
+                    if (a > 0) && (a < Im.x) && (b > 0) && (b <Im.y)
                         if p(a,b,r) < maxScore
                             p(a,b,r) = p(a,b,r)+1;
                             
                         end
-                      end 
-                      
+                    end
+                    
                 end
             end
         end
     end
-p_slice = p(:,:,r);
-maxVal(r) = max(p_slice(:));  
-
-maxArray = imregionalmax(p_slice);
-maxArray = maxArray & p_slice >= (maxVal(r)*0.5) ;
-
- 
-
+    
+    p_slice = p(:,:,r);
+    maxVal(r) = max(p_slice(:));
+    
 end
 
-[~,locations] = findpeaks(p_slice,2,'MinPeakProminence',4,'Threshold',5);
-
-figure
-imshow(I_GSC)
-hold on
+[~,locations] = findpeaks(maxVal,'MinPeakProminence',4,'Threshold',5);
 
 
-
-
-for x = 1:length(locations)
+for x = 1:numel(locations)
     p_prime = p(:,:,locations(x));
-    [~,ind] = max(p_prime(:));
-    [row,col] = ind2sub(size(p_prime),ind);
-    plot (col,row,'.','markersize',10)
-    plotCircle(col,row,locations(x));
+    a= max(p_prime(:));
+    centers = centers + (p_prime > (0.8*a));
 end
 
+% If you Imshow(ab) you can see the centers, the issues is there are 15 pts
+% (some are grouped) and I need to eliminate the false centers without
+% removing the actual centers
 
+
+
+%figure
+%imshow(I_GSC)
+%hold on
 
 function plotCircle(x,y,r)
 ang=0:360; 
